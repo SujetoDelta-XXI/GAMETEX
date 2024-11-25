f23732711b024382d8160bd95066d0368872f967
axel <axtev2000@hotmail.com>
2024-11-24 22:35:35 -0500

    a

--- resources/views/components/authentication-card-logo.blade.php
+++ resources/views/components/authentication-card-logo.blade.php
@@ -1,4 +1,4 @@
-<a href="/">
+<a href="/" >
     <svg class="size-16" viewbox="0 0 48 48" fill="none" xmlns="http://www.w3.org/2000/svg">
         <img class="h-12" src="{{ asset('logo.png') }}">
     </svg>
