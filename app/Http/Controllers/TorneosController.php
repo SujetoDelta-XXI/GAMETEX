f385ba7def4301919746b6e56ce808d5451bc7d6
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2024-11-24 22:18:13 -0500

    Index - Parte II (interfaz torneos)

---
+++ app/Http/Controllers/TorneosController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class TorneosController extends Controller
+{
+    public function index()
+    {
+        return view('torneos.index');
+    }
+}
