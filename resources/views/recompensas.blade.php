9a4ee8c3440f86c1427d04a744aae6dd124d98cf
SujetoDelta-XXI <carlos.asparrin@tecsup.edu.pe>
2024-11-27 18:04:23 -0500

    a

--- resources/views/recompensas.blade.php
+++ resources/views/recompensas.blade.php
@@ -12,7 +12,6 @@
                             <span class="text-gray-500">Todas la recompensas:</span>
                             <span class="dark:text-white">123456</span>
                         </h5>
-                        <h5 class="text-gray-500 dark:text-gray-400 ml-1">1-100 (436)</h5>
                         <button type="button" class="group" data-tooltip-target="results-tooltip">
                             <svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg"
                                 class="h-4 w-4 text-gray-400 group-hover:text-gray-900 dark:group-hover:text-white"
@@ -21,13 +20,7 @@
                                     d="M18 10a8 8 0 11-16 0 8 8 0 0116 0zm-7-4a1 1 0 11-2 0 1 1 0 012 0zM9 9a1 1 0 000 2v3a1 1 0 001 1h1a1 1 0 100-2v-3a1 1 0 00-1-1H9z"
                                     clip-rule="evenodd" />
                             </svg>
-                            <span class="sr-only">Más información</span>
                         </button>
-                        <div id="results-tooltip" role="tooltip"
-                            class="absolute z-10 invisible inline-block px-3 py-2 text-sm font-medium text-white transition-opacity duration-300 bg-gray-900 rounded-lg shadow-sm opacity-0 tooltip dark:bg-gray-700">
-                            Showing 1-100 of 436 results
-                            <div class="tooltip-arrow" data-popper-arrow=""></div>
-                        </div>
                     </div>
                 </div>
                 <div
@@ -121,15 +114,10 @@
                                                 <input id="apple" type="checkbox" value=""
                                                     class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
                                                 <label for="apple"
-                                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Apple
-                                                    (56)</label>
-                                            </li>
-                                            <li class="flex items-center">
-                                                <input id="razor" type="checkbox" value=""
-                                                    class="w-4 h-4 bg-gray-100 border-gray-300 rounded text-primary-600 focus:ring-primary-500 dark:focus:ring-primary-600 dark:ring-offset-gray-700 focus:ring-2 dark:bg-gray-600 dark:border-gray-500">
-                                                <label for="razor"
-                                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">Razor
-                                                    (49)</label>         
+                                                    class="ml-2 text-sm font-medium text-gray-900 dark:text-gray-100">
+                                                    Apple(56)
+                                                </label>
+                                            </li>       
                                         </ul>
                                     </div>
                                 </div>
@@ -149,21 +137,16 @@
                                     </button>
                                 </h2>
                                 <div id="price-body" class="hidden" aria-labelledby="price-heading">
-                                    <div
-                                        class="flex items-center py-2 space-x-3 font-light border-b border-gray-200 dark:border-gray-600">
-                                        <select id="price-from"
-                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                            <option disabled="" selected="">Mayor</option>
-                                            <option>$500</option>
-                                            <option>$2500</option>
-                                            <option>$5000</option>
-                                        </select><select id="price-to"
-                                            class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
-                                            <option disabled="" selected="">Menos</option>
-                                            <option>$500</option>
-                                            <option>$2500</option>
-                                            <option>$5000</option>
-                                        </select></div>
+                                    <div class="flex items-center py-2 space-x-3 font-light border-b border-gray-200 dark:border-gray-600">
+                                        <label class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                            Menor
+                                        </label>
+                                        <input type="number">
+                                        <label class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                                            Menor
+                                        </label>
+                                        <input type="number">
+                                    </div>
                                 </div>
                             </div>
                         </div>
@@ -204,109 +187,7 @@
                                 16/11/2024
                             </x-slot>
                         </x-admin.recompensas_list>
-                        <x-admin.recompensas_list>
-                            <x-slot name="imagen">
-                                https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-front-image.png
-                            </x-slot>
-                            <x-slot name="producto">
-                                Apple
-                            </x-slot>
-                            <x-slot name="categoria">
-                                PC
-                            </x-slot>
-                            <x-slot name="stock">
-                                15
-                            </x-slot>
-                            <x-slot name="precio">
-                                20
-                            </x-slot>
-                            <x-slot name="f_create">
-                                12/10/2024
-                            </x-slot>
-                            <x-slot name="f_update">
-                                16/11/2024
-                            </x-slot>
-                        </x-admin.recompensas_list>
-                        <x-admin.recompensas_list>
-                            <x-slot name="imagen">
-                                https://flowbite.s3.amazonaws.com/blocks/application-ui/products/imac-front-image.png
-                            </x-slot>
-                            <x-slot name="producto">
-                                Apple
-                            </x-slot>
-                            <x-slot name="categoria">
-                                PC
-                            </x-slot>
-                            <x-slot name="stock">
-                                15
-                            </x-slot>
-                            <x-slot name="precio">
-                                20
-                            </x-slot>
-                            <x-slot name="f_create">
-                                12/10/2024
-                            </x-slot>
-                            <x-slot name="f_update">
-                                16/11/2024
-                            </x-slot>
-                        </x-admin.recompensas_list>
                     </table>
-                    
-                <nav class="flex flex-col md:flex-row justify-between items-start md:items-center space-y-3 md:space-y-0 p-4"
-                    aria-label="Table navigation">
-                    <span class="text-sm font-normal text-gray-500 dark:text-gray-400">
-                        Showing
-                        <span class="font-semibold text-gray-900 dark:text-white">1-10</span>
-                        of
-                        <span class="font-semibold text-gray-900 dark:text-white">1000</span>
-                    </span>
-                    <ul class="inline-flex items-stretch -space-x-px">
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center h-full py-1.5 px-3 ml-0 text-gray-500 bg-white rounded-l-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                                <span class="sr-only">Previous</span>
-                                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">1</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">2</a>
-                        </li>
-                        <li>
-                            <a href="#" aria-current="page"
-                                class="flex items-center justify-center text-sm z-10 py-2 px-3 leading-tight text-primary-600 bg-primary-50 border border-primary-300 hover:bg-primary-100 hover:text-primary-700 dark:border-gray-700 dark:bg-gray-700 dark:text-white">3</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">...</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center text-sm py-2 px-3 leading-tight text-gray-500 bg-white border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">100</a>
-                        </li>
-                        <li>
-                            <a href="#"
-                                class="flex items-center justify-center h-full py-1.5 px-3 leading-tight text-gray-500 bg-white rounded-r-lg border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white">
-                                <span class="sr-only">Next</span>
-                                <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewbox="0 0 20 20"
-                                    xmlns="http://www.w3.org/2000/svg">
-                                    <path fill-rule="evenodd"
-                                        d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z"
-                                        clip-rule="evenodd" />
-                                </svg>
-                            </a>
-                        </li>
-                    </ul>
-                </nav>
             </div>
         </div>
     </section>
@@ -329,9 +210,10 @@
                                 d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
                                 clip-rule="evenodd" />
                         </svg>
-                        <span class="sr-only">Close modal</span>
+                        <span class="sr-only">Cerrar Modal</span>
                     </button>
                 </div>
+
                 <!-- Modal body -->
                 <form action="#">
                     <div class="grid gap-4 mb-4 sm:grid-cols-2">
@@ -581,45 +463,8 @@
                         d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
                         clip-rule="evenodd" />
                 </svg>
-                Delete
+                Eliminar
             </button>
         </div>
     </form>
-
-    <!-- Delete Modal -->
-    <div id="delete-modal" tabindex="-1"
-        class="fixed top-0 left-0 right-0 z-50 hidden p-4 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] max-h-full">
-        <div class="relative w-full h-auto max-w-md max-h-full">
-            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
-                <button type="button"
-                    class="absolute top-3 right-2.5 text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center dark:hover:bg-gray-800 dark:hover:text-white"
-                    data-modal-toggle="delete-modal">
-                    <svg aria-hidden="true" class="w-5 h-5" fill="currentColor" viewbox="0 0 20 20"
-                        xmlns="http://www.w3.org/2000/svg">
-                        <path fill-rule="evenodd"
-                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                            clip-rule="evenodd" />
-                    </svg>
-                    <span class="sr-only">Close modal</span>
-                </button>
-                <div class="p-6 text-center">
-                    <svg aria-hidden="true" class="mx-auto mb-4 text-gray-400 w-14 h-14 dark:text-gray-200"
-                        fill="none" stroke="currentColor" viewbox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                            d="M12 8v4m0 4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
-                    </svg>
-                    <h3 class="mb-5 text-lg font-normal text-gray-500 dark:text-gray-400">Are you sure you want to delete
-                        this product?</h3>
-                    <button data-modal-toggle="delete-modal" type="button"
-                        class="text-white bg-red-600 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 dark:focus:ring-red-800 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2">Yes,
-                        I'm sure</button>
-                    <button data-modal-toggle="delete-modal" type="button"
-                        class="text-gray-500 bg-white hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 rounded-lg border border-gray-200 text-sm font-medium px-5 py-2.5 hover:text-gray-900 focus:z-10 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-gray-600">No,
-                        cancel</button>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/flowbite/2.3.0/datepicker.min.js"></script>
 @endsection
