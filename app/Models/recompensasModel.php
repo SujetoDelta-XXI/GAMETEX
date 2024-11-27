d8b12b6ea0a188ef1b1ad4fe3130ab2fc11a6725
Brandon Arias Cjuno <brandon12arias@gmail.com>
2024-11-27 17:30:26 -0500

    solucion de conflictos

--- app/Models/recompensasModel.php
+++ app/Models/recompensasModel.php
@@ -2,25 +2,53 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class recompensasModel extends Model
+class RecompensasModel extends Model
 {
+    use HasFactory;
+
+    // Nombre de la tabla (opcional si sigue la convención pluralizada)
     protected $table = 'recompensas';
+
+    // Campos asignables masivamente
     protected $fillable = [
-        'id',
         'nombre',
         'cantidad',
+        'precio',
+        'recompensa_tipo_id',
+        'torneo_id',
+        /* 'evento_id', */
     ];
-    public function recompensasTipo() 
-    { 
-        return $this->belongsTo(recompensasTipoModel::class, 'recompensas_tipo_id'); 
+
+    protected $cast = [
+        'created_at' => 'datetime',
+        'updated_at' => 'datetime'
+    ];
+
+
+    /**
+     * Relación con RecompensasTipo (recompensas_tipo).
+     */
+    public function tipo()
+    {
+        return $this->belongsTo(RecompensasTipoModel::class, 'recompensa_tipo_id');
     }
-    public function torneo() { 
-        return $this->belongsTo(torneoModel::class, 'torneo_id'); 
-    } 
-    public function evento() { 
+
+    /**
+     * Relación con Torneo (torneos).
+     */
+    public function torneo()
+    {
+        return $this->belongsTo(TorneosModel::class, 'torneo_id');
+    }
+
+    /**
+     * Relación con Evento (eventos).
+     */
+/*     public function evento()
+    {
         return $this->belongsTo(eventosModel::class, 'evento_id');
-    }
-    
+    } */
 }
